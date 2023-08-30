54442c061028a619ca8257d0766324b76e699e9a
pimentacaroline <pimenta.caroline@gmail.com>
2023-08-30 19:05:16 +0200

    Rendered data from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,49 +1,40 @@
-import {useState} from 'react';
-import {MovieCard} from '../movie-card/movie-card';
-import {MovieView} from '../movie-view/movie-view';
+import { useEffect, useState } from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 
 export const MainView = () => {
-	const [movies, setMovies] = useState([
-		{
-			id: 1, 
-			title:'Silence of the Lambs',
-			description: 'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.',
-			genre: 'Thriller',
-			director: 'Jonathan Demme',
-			image: 'https://www.themoviedb.org/t/p/original/uS9m8OBk1A8eM9I042bx8XXpqAq.jpg'
-		},
-		{
-			id: 2, 
-			title: 'Fantastic Mr. Fox',
-			description: 'An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers\' retaliation.',
-			genre: 'Animation',
-			director: 'Wes Anderson',
-			image: 'https://www.themoviedb.org/t/p/original/izBLrCxhSZRwNXuwp1GAR3wmInL.jpg'
-		},
-		{
-			id: 3, 
-			title: 'Underground',
-			description: 'A group of Serbian socialists prepares for the war in a surreal underground filled by parties, tragedies, love, and hate.',
-			genre: 'Drama',
-			director: 'Emir Kusturica',
-			image: 'https://www.themoviedb.org/t/p/original/h8N6y13t4VusrDdH5PzTkwvBvgN.jpg'
-		},
-		{
-			id: 4, 
-			title: 'The Grand Budapest Hotel',
-			description: 'A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel\'s glorious years under an exceptional concierge.',
-			genre: 'Comedy',
-			director: 'Wes Anderson',
-			image: 'https://www.themoviedb.org/t/p/original/eWdyYQreja6JGCzqHWXpWHDrrPo.jpg'
-		},
-	]);
+	const [movies, setMovies] = useState([]);
 
 	const [selectedMovie, setSelectedMovie] = useState(null);
 
+	useEffect(() => {
+		fetch("https://cp-movies-api-41b2d280c95b.herokuapp.com/movies")
+			.then((response) => response.json())
+			.then((data) => {
+				const moviesFromApi = data.map((movie) => {
+					return {
+						_id: movie.id,
+						Title: movie.Title,
+						ImagePath: movie.ImagePath,
+						Description: movie.Description,
+						Genre: {
+							name: movie.Genre.Name
+						},
+						Director: {
+							Name: movie.Director.Name
+						},
+						Featured: movie.Featured
+					};
+				});
+
+				setMovies(moviesFromApi);
+			});
+	}, []);
+
 	if (selectedMovie) {
 		return (
-			<MovieView movie={selectedMovie} onBackClick={()=> setSelectedMovie(null)}/>
+			<MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
 		);
 	}
 
@@ -54,14 +45,14 @@ export const MainView = () => {
 	return (
 		<div>
 			{movies.map((movie) => (
-        <MovieCard 
-					key={movie.id} 
+				<MovieCard
+					key={movie._id}
 					movie={movie}
 					onMovieClick={(newSelectedMovie) => {
 						setSelectedMovie(newSelectedMovie);
 					}}
 				/>
-      ))}
+			))}
 		</div>
 	);
 };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const MovieCard = ({movie, onMovieClick}) => {
 	return (
 		<div
@@ -5,7 +7,15 @@ export const MovieCard = ({movie, onMovieClick}) => {
 				onMovieClick(movie);
 			}}
 		>
-			{movie.title}
+			{movie.Title}
 		</div>
 	);
+};
+
+MovieCard.propTypes = {
+	movie: PropTypes.shape ({
+		title: PropTypes.string.isRequired,
+		image: PropTypes.string.isRequired
+	}).isRequired,
+	onMovieClick: PropTypes.func.isRequired
 };
