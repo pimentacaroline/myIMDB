fa32f886492f5a0729c35d3b69dc549bc833dc17
pimentacaroline <pimenta.caroline@gmail.com>
2023-08-31 13:58:19 +0200

    Created signup view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,38 +2,43 @@ import { useEffect, useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
+
 
 export const MainView = () => {
 	const storedUser = JSON.parse(localStorage.getItem("user"));
 	const storedToken = localStorage.getItem("token");
-	const [user, setUser] = useState(storedUser? storedUser:null);
-	const [token, setToken] = useState(storedToken? storedToken:null);
+	const [user, setUser] = useState(storedUser ? storedUser : null);
+	const [token, setToken] = useState(storedToken ? storedToken : null);
 	const [movies, setMovies] = useState([]);
 	const [selectedMovie, setSelectedMovie] = useState(null);
 
 
 	useEffect(() => {
-    if (!token) return;
+		if (!token) return;
 
-    fetch("https://cp-movies-api-41b2d280c95b.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then((response) => response.json())
-      .then((movies) => {
-        setMovies(movies);
-      });
-  }, [token]);
+		fetch("https://cp-movies-api-41b2d280c95b.herokuapp.com/movies", {
+			headers: { Authorization: `Bearer ${token}` }
+		})
+			.then((response) => response.json())
+			.then((movies) => {
+				setMovies(movies);
+			});
+	}, [token]);
 
 	if (!user) {
-    return (
-			<LoginView 
-				onLoggedIn={(user, token) => {
-					setUser(user);
-					setToken(token);
-				}}
-			/>
+		return (
+			<>
+				<LoginView
+					onLoggedIn={(user, token) => {
+						setUser(user);
+						setToken(token);
+				}}/>
+				or
+				<SignupView />
+			</>
 		);
-  }
+	}
 
 	if (selectedMovie) {
 		return (
@@ -45,21 +50,21 @@ export const MainView = () => {
 		return <div>The list is empty!</div>
 	}
 
-  return (
-    <div>
-      <button onClick={() => {setUser(null); setToken(null); localStorage.clear();}}>
-        Logout
-      </button>
-      {books.map((book) => (
-        <BookCard
-          key={book.id}
-          book={book}
-          onBookClick={(newSelectedBook) => {
-            setSelectedBook(newSelectedBook);
-          }}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			<button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>
+				Logout
+			</button>
+			{books.map((book) => (
+				<BookCard
+					key={book.id}
+					book={book}
+					onBookClick={(newSelectedBook) => {
+						setSelectedBook(newSelectedBook);
+					}}
+				/>
+			))}
+		</div>
+	);
 };
 
