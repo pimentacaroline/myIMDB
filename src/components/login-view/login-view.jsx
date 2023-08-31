fbd818501cb6995c77d6f1e9b39bedf5bf9dd84f
pimentacaroline <pimenta.caroline@gmail.com>
2023-08-31 11:01:17 +0200

    implemented the login form and user verification

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,51 @@
-export const LoginView = () => {
-  return (
-    <form>
+import React from 'react';
+import {useState} from 'react';
+
+
+export const LoginView = ({onLoggedIn}) => {
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+		event.preventDefault();
+
+		const data = {
+			access: username,
+			secret: password
+		};
+
+		fetch('https://cp-movies-api-41b2d280c95b.herokuapp.com/login.json', {
+			method: 'POST',
+			body: JSON.stringify(data)
+		}).then((response) => {
+			if (response.ok) {
+				onLoggedIn(username);
+			} else {
+				alert('Login failed');
+			}
+		});
+	};
+  
+	return (
+    <form onSubmit={handleSubmit}>
       <label>
         Username:
-        <input type="text" />
+        <input 
+				type="text"
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+				/>
       </label>
       <label>
         Password:
-        <input type="password" />
+        <input 
+				type="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+				/>
       </label>
       <button type="submit">Submit</button>
     </form>
   );
 };
+
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
